86317011ccde474867c1c7c2de1d5044b070aeb7
wakaba ohahsi <wakaba@wakaba-no-MacBook-Pro.local>
2015-07-01 21:58:31 +0900

    完走！！

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require 'sinatra/reloader' if development?
 require './models/item.rb'
 
 get '/' do
-  @items = Item.all
+  @items = Item.all.order(paid_at: :desc)
+  @graph_items = Item.all.order(category_id: :asc)
   @total = -(Item.sum(:price) - 2 * Item.where(:category_id => 1).sum(:price))
   # @total = Item.sum(:price)
   @categories = Category.all
@@ -28,25 +29,27 @@ get '/category/:id' do
   @category = Category.find(params[:id])
   @category_name  =@category.name
   @items = @category.items
+  @graph_items = @category.items
+  @total = -(@items.sum(:price) - 2 * @items.where(:category_id => 1).sum(:price))
   erb:index
 end
 
-get '/' do
-  @items = Item.all
-  @total = Item.sum(:price)
-  @categories = Category.all
-  erb:index
-end
+# get '/' do
+#   @items = Item.all.order(paid_at: :desc)
+#   @total = Item.sum(:price)
+#   @categories = Category.all
+#   erb:index
+# end
 
-post '/create' do
-  Item.create({
-      title:params[:title],
-      price:params[:price],
-      category_id:params[:category]
-  })
-
-  redirect '/'
-end
+# post '/create' do
+#   Item.create({
+#       title:params[:title],
+#       price:params[:price],
+#       category_id:params[:category]
+#   })
+#
+#   redirect '/'
+# end
 
 get '/category/:id' do
   @categories    = Category.all
@@ -59,6 +62,14 @@ get '/category/:id' do
   erb:index
 end
 
+get '/date/:date' do
+  @categories    = Category.all
+  @items = Item.where(:paid_at => params[:date])
+  @graph_items = Item.where(:paid_at => params[:date]).order(category_id: :asc)
+  @total = -(@items.sum(:price) - 2 * @items.where(:category_id => 1).sum(:price))
+  erb :index
+end
+
 post '/delete/:id' do
   Item.find(params[:id]).destroy
   redirect '/'
@@ -66,6 +77,7 @@ end
 
 post '/edit/:id' do
   @item = Item.find(params[:id])
+  @categories    = Category.all
   erb:edit
 end
 
@@ -73,7 +85,8 @@ post '/renew/:id' do
   @item = Item.find(params[:id])
   @item.update({
     title:params[:title],
-    price:params[:price]
+    price:params[:price],
+    category_id: params[:category],
   })
   redirect '/'
 end
